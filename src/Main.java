28a1f7478742eda9ab120af18cfe6e09fdc797f7
sofia va <sophia.lily.lee@gmail.com>
2024-08-15 11:42:09 +0300

    Bonus and balance count

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int initialBalance = 100;
+        int topUpSum = 1100;
+        int oneBonus = 100;
+
+        int finalBalance;
+        int bonuses;
+        if (topUpSum >= 1000) {
+            bonuses = topUpSum / oneBonus;
+            finalBalance = initialBalance + topUpSum + bonuses;
+        } else {
+            bonuses = 0;
+            finalBalance = initialBalance + topUpSum;
+        }
+        System.out.println("Итоговый баланс: " + finalBalance);
+        System.out.println("Количество начисленных бонусов: " + bonuses);
+
     }
 }
